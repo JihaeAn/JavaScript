53aca626c75e176b2c10eb0330bb883f0f32473f
jihae <dkswlgo6615@naver.com>
2024-04-13 16:17:37 +0900

    Function

--- basic/11_function.js
+++ basic/11_function.js
@@ -31,3 +31,77 @@ function calculate(number){
  * 실제 입력하는 값은 argument라고 한다.
  */
 calculate(4);
+calculate(5);
+
+function multiply(x, y){
+    console.log(x * y);
+}
+
+multiply(2,4);
+
+function multiply(x, y = 10){
+    console.log(x * y);
+}
+
+multiply(2, 4);     // y의 값이 있으니 그대로 2 * 4 실행
+multiply(2);           // y의 값이 없으니 디폴트값인 10이 들어가서 2 * 10 실행
+
+/**
+ * 반환 받기
+ * return 받기
+ */
+console.log('--------------------------');
+function multiply(x, y) {
+    return x * y;
+}
+
+const result1 = multiply(2, 4);
+console.log(result1);
+
+const result2 = multiply(4, 5);
+console.log(result2);
+
+/**
+ * Arrow 함수
+ */
+const multiply2 = (x, y) => {
+    return x * y;
+}
+console.log(multiply(3, 4));
+
+const multiply3 = (x, y) => x * y;
+console.log(multiply3(4,5));
+
+const multiply4 = x => x * 2;
+console.log(multiply4(2));
+
+console.log('=====================');
+// 밑에 두 개는 같은 함수라고 보면 됨
+const multiply5 = x => y => z => `x: ${x} y: ${y} z: ${z}`;
+console.log(multiply5(2)(5)(7));
+
+function multiply6(x){
+    return function(y){
+        return function(z){
+            return `x: ${x} y: ${y} z: ${z}`;
+        }
+    }
+}
+console.log(multiply6(3)(4)(5));
+
+const multiplyTwo = function(x, y){
+    return x * y;
+}
+console.log(multiplyTwo(4, 5));
+
+
+
+
+
+
+
+
+
+
+
+
