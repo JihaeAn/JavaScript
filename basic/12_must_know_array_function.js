05f072e9aa979f10ad847fd72697892ab5a260ca
jihae <dkswlgo6615@naver.com>
2024-04-17 21:28:16 +0900

    Must_know_array_function

---
+++ basic/12_must_know_array_function.js
@@ -0,0 +1,17 @@
+/**
+ * Array Function
+ */
+let iveMembers = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '이서',
+    '리즈'
+];
+console.log(iveMembers);
+
+// push()
+// push()는 반환값을 가지는데 배열의 길이를 반환해준다.
+console.log(iveMembers.push('코드팩토리'));  // 7
+console.log(iveMembers);
