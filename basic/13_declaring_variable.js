03cdc3a185dee70f8b126d7684d5d8bf478df233
jihae <dkswlgo6615@naver.com>
2024-04-22 21:09:03 +0900

    declaring_variable

---
+++ basic/13_declaring_variable.js
@@ -0,0 +1,16 @@
+/**
+ * Object 객체
+ */
+
+// key : value pair
+let yuJin = {
+    name: '안유진',
+    group: '아이브',
+    dance: function (){
+        return '안유진이 춤을 춥니다.';
+    }
+};
+
+console.log(yuJin);
+console.log(yuJin.name);
+console.log(yuJin['name']);
