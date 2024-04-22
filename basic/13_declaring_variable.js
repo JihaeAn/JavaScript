99f66ff057f224a294fdf18dc599bc9473dd7db0
jihae <dkswlgo6615@naver.com>
2024-04-22 21:55:10 +0900

    declaring_variable

--- basic/13_declaring_variable.js
+++ basic/13_declaring_variable.js
@@ -7,10 +7,74 @@ let yuJin = {
     name: '안유진',
     group: '아이브',
     dance: function (){
-        return '안유진이 춤을 춥니다.';
+        return `걸그룹 ${this.group} 멤버인 ${this.name}이 춤을 춥니다.`;
     }
 };
 
-console.log(yuJin);
+console.log(yuJin);  // 댄스는 이렇게 출력 => dance: [Function: dance]
 console.log(yuJin.name);
-console.log(yuJin['name']);
+console.log(yuJin['name']);     // 위와 같은 의미임
+
+const key = 'name';
+
+console.log(yuJin[key]);
+
+console.log(yuJin.dance());
+
+const nameKey = 'name';
+const nameValue = '안유진';
+
+const groupKey ='group';
+const groupValue = 'ive';
+
+const yuJin2 = {
+    [nameKey]: nameValue,
+    [groupKey]: groupValue,
+    dance: function (){
+        return `${this.name}이 춤을 춥니다.`;
+    }
+}
+console.log(yuJin2);    // { name: '안유진', group: 'ive', dance: [Function: dance] }
+console.log(yuJin2.dance());
+
+yuJin2['group'] = '뉴진스';    // 값이 수정됨
+console.log(yuJin2);
+
+yuJin2['englishName'] = 'An Yu Jin';
+console.log(yuJin2); // 존재하지 않는 키 englishName도 추가되어서 새로운 property가 출력이 됨
+
+delete yuJin2['englishName'];
+console.log(yuJin2);
+
+// const 키워드를 쓰면은 값을 변경할 수 없다고 했는데, 계속 변경이 되고 있다. 왤까?
+
+/**
+ * 객체의 특징
+ *
+ * 1) const로 선언할 경우 객체 자체를 변경할 수는 없다.
+ * 2) 객체 안의 프로퍼티나 메서드는 변경할 수 있다.
+ */
+const wonYoung = {
+    name: '장원영',
+    group: '아이브',
+}
+console.log(wonYoung);
+
+// 이미 초기화가 되면 값을 지울 수가 없다.
+// wonYoung = {};
+
+wonYoung['group'] = '르세라핌';
+console.log(wonYoung);
+wonYoung.group = '안지해와 아이들';
+console.log(wonYoung);
+
+
+
+
+
+
+
+
+
+
+
