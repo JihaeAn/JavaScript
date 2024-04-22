57f39794e28cb91d0599b470d5af59fa22f89a67
jihae <dkswlgo6615@naver.com>
2024-04-22 23:00:18 +0900

    Copy Value and Reference

--- basic/13_declaring_variable.js
+++ basic/13_declaring_variable.js
@@ -15,6 +15,11 @@ console.log(yuJin);  // 댄스는 이렇게 출력 => dance: [Function: dance]
 console.log(yuJin.name);
 console.log(yuJin['name']);     // 위와 같은 의미임
 
+// 응용: in을 쓰면 key값을 가져온다.
+for(let key in yuJin){
+    console.log(`${key}: ${yuJin[key]}`)
+}
+
 const key = 'name';
 
 console.log(yuJin[key]);
@@ -49,7 +54,7 @@ console.log(yuJin2);
 // const 키워드를 쓰면은 값을 변경할 수 없다고 했는데, 계속 변경이 되고 있다. 왤까?
 
 /**
- * 객체의 특징
+ * const로 선언한 객체의 특징
  *
  * 1) const로 선언할 경우 객체 자체를 변경할 수는 없다.
  * 2) 객체 안의 프로퍼티나 메서드는 변경할 수 있다.
@@ -61,12 +66,33 @@ const wonYoung = {
 console.log(wonYoung);
 
 // 이미 초기화가 되면 값을 지울 수가 없다.
-// wonYoung = {};
+// wonYoung = { };
+delete wonYoung.name;
+delete wonYoung.group;
+console.log(wonYoung);
 
+// 객체 안 프로퍼티나 메서드는 변경 가능 !
 wonYoung['group'] = '르세라핌';
 console.log(wonYoung);
 wonYoung.group = '안지해와 아이들';
 console.log(wonYoung);
+
+/**
+ * 모든 키값 다 가져오기
+ */
+console.log(Object.keys(wonYoung));
+
+/**
+ * 모든 벨류 값 다 가져오기
+ */
+console.log(Object.values(wonYoung));
+
+const name = '안지해';
+const jiHae = {
+    name,           // name: name이랑 똑같음
+};
+console.log(jiHae); // { name: '안지해' }
+
 
 
 
---
+++ basic/14_copy_value_and_reference.js
@@ -0,0 +1,74 @@
+/**
+ * copy by value 값에 의한 전달
+ * copy by reference 참조에 의한 전달
+ *
+ * 1) 기본적으로 모든 primitive 값은 copy by value
+ * 2) 객체는 copy by reference
+ */
+let original = '안녕하세요';
+let clone = original;
+
+console.log(original);
+console.log(clone);
+
+clone += ' 안지해 입니다.';
+console.log(original);
+console.log(clone);
+
+let originalObj = {
+    name: '안유진',
+    group: '아이브',
+};
+let cloneObj = originalObj;
+
+console.log(originalObj);
+console.log(cloneObj);
+
+
+console.log('========================');
+
+
+originalObj['group'] = '뉴진스';
+console.log(originalObj);
+console.log(cloneObj);      // 둘이 같은 값 출력
+
+console.log(originalObj === cloneObj);  // true
+console.log(original === clone);        // false
+
+// Object 안에 값이 같다고 해서 같은 값이 아님
+originalObj = {
+    name: '최지호',
+    group: '코드팩토리',
+}
+cloneObj = {
+    name: '최지호',
+    group: '코드팩토리',
+}
+console.log(originalObj === cloneObj);  // false
+
+/**
+ * Copy by Value
+ * 값 그대로를 복사
+ *
+ * Copy by Reference
+ * 참조 객체 공간(주소)을 복사
+ *  => 그러니 오리지널 값이 바뀌면 참조하고 있는 모든 값들도 변함!
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
